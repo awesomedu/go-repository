974a6529f4c970b8f92c40a8c3c7f83908d23902
唐都 <7277710@qq.com>
2019-01-21 16:47:12 +0800

    1.常量const 的使用以及const 多重赋值

---
+++ go5/const.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const  LENGTH int  = 200
+	const WIDTH int  = 3434
+	var area int
+	const a,b,c  = 1,false,"str"
+	area = LENGTH *WIDTH
+	fmt.Println("面积为: : ",area,WIDTH)
+	println()
+	println(a,b,c)
+}
